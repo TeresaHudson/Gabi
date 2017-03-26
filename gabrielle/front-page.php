c9c3c3417c632b044cd23420b1d0bf95a211909a
Teresa Hudson <teresa@tkhcreates.com>
2017-03-25 17:08:56 -0700

    Updated typography

--- gabrielle/front-page.php
+++ gabrielle/front-page.php
@@ -18,21 +18,111 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 
-			<div class="main-img-wrap" style="background: blue">
+			<div class="main-img-wrap">
 				<div class="row">
 					<div class="small-12 medium-expand columns">
 						Nothing Goes in Here. 
 					</div>
 					<div class="small-12 medium-expand columns">
 						<h2>Title</h2>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare hendrerit ipsum, finibus aliquam tortor faucibus et. Etiam vitae iaculis quam. Nulla ut ullamcorper lorem, facilisis tempus metus. Cras maximus leo quis lacinia molestie. Praesent malesuada ligula vitae elit sodales fermentum.
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare hendrerit ipsum, finibus aliquam tortor faucibus et. Etiam vitae iaculis quam. Nulla ut ullamcorper lorem, facilisis tempus metus. Cras maximus leo quis lacinia molestie. Praesent malesuada ligula vitae elit sodales fermentum.</p>
 						<a>Call to action</a>
 					</div>
 				</div>
 			</div>
 
-			<div class="testimonials-wrap" style="background: green">
+			<div class="testimonials-wrap">
 				<div class="row">
+
+					<div class="small-12">
+						<h1>Header H1 is Fantastic.</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare hendrerit ipsum, finibus aliquam tortor faucibus et. Etiam vitae iaculis quam. Nulla ut ullamcorper lorem, facilisis tempus metus. Cras maximus leo quis lacinia molestie. Praesent malesuada ligula vitae elit sodales fermentum.</p>
+
+						<h2>Header H2 is Just the Right Size</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare hendrerit ipsum, finibus aliquam tortor faucibus et. Etiam vitae iaculis quam. Nulla ut ullamcorper lorem, facilisis tempus metus. Cras maximus leo quis lacinia molestie. Praesent malesuada ligula vitae elit sodales fermentum.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare hendrerit ipsum, finibus aliquam tortor faucibus et. Etiam vitae iaculis quam. Nulla ut ullamcorper lorem, facilisis tempus metus. Cras maximus leo quis lacinia molestie. Praesent malesuada ligula vitae elit sodales fermentum.</p>
+
+						<h3>Header H3 is Less Likely to be Used but is Still Cute.</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare hendrerit ipsum, finibus aliquam tortor faucibus et. Etiam vitae iaculis quam. Nulla ut ullamcorper lorem, facilisis tempus metus. Cras maximus leo quis lacinia molestie. Praesent malesuada ligula vitae elit sodales fermentum.</p>
+
+						<ul>
+							<li>This is a test list</li>
+							<li>Lists have to look good too</li>
+							<li>Lists don't get used much but are super duper important</li>
+						</ul>
+
+						<h4>Does Anyone Ever Use H4?</h4>
+						<pL>orem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare hendrerit ipsum, finibus aliquam tortor faucibus et. Etiam vitae iaculis quam. Nulla ut ullamcorper lorem, facilisis tempus metus. Cras maximus leo quis lacinia molestie. Praesent malesuada ligula vitae elit sodales fermentum.</p>
+						<ol>
+							<li>Ordered Lists Should look a lot like Unordered Lists</li>
+							<li>Ordered LIsts don't get used too often though</li>
+							<li>I need a third item so here I am</li>
+						</ol>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare hendrerit ipsum, finibus aliquam tortor faucibus et. Etiam vitae iaculis quam. Nulla ut ullamcorper lorem, facilisis tempus metus. Cras maximus leo quis lacinia molestie. Praesent malesuada ligula vitae elit sodales fermentum.</p>
+
+						<h5>No One Uses H5 But I am Styling it Anyway</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare hendrerit ipsum, finibus aliquam tortor faucibus et. Etiam vitae iaculis quam. Nulla ut ullamcorper lorem, facilisis tempus metus. Cras maximus leo quis lacinia molestie. Praesent malesuada ligula vitae elit sodales fermentum.</p>
+						
+						<table>
+							<caption>This is the Table Caption</caption>
+							<thead>
+								<tr>
+									<th>Col 1 Header</th>
+									<th>Col 2 header</th>
+									<th>Col 3 Header</th>
+								</tr>
+							</thead>
+							<tr>
+								<td>Cell 1</td>
+								<td>Cell 3</td>
+								<td>Cell 3</td>
+							</tr>
+
+							<tr>
+								<td>Cell 4</td>
+								<td>Cell 5</td>
+								<td>Cell 6</td>
+							</tr>
+
+							<tr>
+								<td>Cell 7</td>
+								<td>Cell 8</td>
+								<td>Cell 9</td>
+							</tr>
+						</table>
+
+						<table class="center zebra">
+							<caption>This is the Table Caption</caption>
+							<thead>
+								<tr>
+									<th>Col 1 Header</th>
+									<th>Col 2 header</th>
+									<th>Col 3 Header</th>
+								</tr>
+							</thead>
+							<tr>
+								<td>Cell 1</td>
+								<td>Cell 3</td>
+								<td>Cell 3</td>
+							</tr>
+
+							<tr>
+								<td>Cell 4</td>
+								<td>Cell 5</td>
+								<td>Cell 6</td>
+							</tr>
+
+							<tr>
+								<td>Cell 7</td>
+								<td>Cell 8</td>
+								<td>Cell 9</td>
+							</tr>
+						</table>
+
+						<h6>I have Never Used H6. Ever.</h6>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare hendrerit ipsum, finibus aliquam tortor faucibus et. Etiam vitae iaculis quam. Nulla ut ullamcorper lorem, facilisis tempus metus. Cras maximus leo quis lacinia molestie. Praesent malesuada ligula vitae elit sodales fermentum.</p>
+					</div>
+
 					<div class="small-12 medium-expand columns">
 						<h2>Testimonial</h2>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare hendrerit ipsum, finibus aliquam tortor faucibus et. Etiam vitae iaculis quam. Nulla ut ullamcorper lorem, facilisis tempus metus. Cras maximus leo quis lacinia molestie. Praesent malesuada ligula vitae elit sodales fermentum.
--- gabrielle/functions.php
+++ gabrielle/functions.php
@@ -102,13 +102,17 @@ add_action( 'widgets_init', 'gabrielle_widgets_init' );
  * Enqueue scripts and styles.
  */
 function gabrielle_scripts() {
-	wp_enqueue_style( 'gabrielle-style', get_stylesheet_uri() );
+	
 
 	wp_enqueue_style( 'gabrielle-style-min', get_stylesheet_uri().'/style.css.min' );
 
 	wp_enqueue_script( 'gabrielle-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'gabrielle-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	wp_enqueue_style( 'gabrielle-style', get_stylesheet_uri() );
+
+	wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Josefin+Sans:300,300i,400,400i,600,700', false );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
