b0d4a69371199552ebb7c8f983a4f4f776de33aa
Teresa Hudson <teresa@tkhcreates.com>
2017-06-11 21:00:14 -0700

    updates

--- gabrielle/functions.php
+++ gabrielle/functions.php
@@ -160,6 +160,33 @@ function gabrielle_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'gabrielle_scripts' );
 
+
+/** Add options page for home page **/
+
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title' 	=> 'Home Page Setup',
+		'menu_title'	=> 'Home Page Setup',
+		'menu_slug' 	=> 'home-page-setup',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+	
+	// acf_add_options_sub_page(array(
+	// 	'page_title' 	=> 'Theme Header Settings',
+	// 	'menu_title'	=> 'Header',
+	// 	'parent_slug'	=> 'theme-general-settings',
+	// ));
+	
+	// acf_add_options_sub_page(array(
+	// 	'page_title' 	=> 'Theme Footer Settings',
+	// 	'menu_title'	=> 'Footer',
+	// 	'parent_slug'	=> 'theme-general-settings',
+	// ));
+	
+}
+
 /**
  * Implement the Custom Header feature.
  */
